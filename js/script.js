709b5cf4e3d6194cd4545d8c49763ac145fccbba
Mitchell <mlbsmith15@hotmail.com>
2018-11-04 16:34:20 -0500

    updated to include email option, font size changes, more mobile friendly layout

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-var counter = 0;
+// window.onscroll = function(ev) {
+//     if (window.pageYOffset > 300) {
+//         counter += 1;
+//         showAboutMe();
+//     } else if (window.pageYOffset < 300 ) {
+//         hideAboutMe();
+//     }
+// }
 
-window.onscroll = function(ev) {
-    if (window.pageYOffset > 300) {
-        counter += 1;
-        showAboutMe();
-    } else if (window.pageYOffset < 300 ) {
-        hideAboutMe();
-    }
+// function showAboutMe() {
+//     document.getElementsByClassName("about").item(0).style.display = "inherit";
+// }
+
+// function hideAboutMe() {
+//     document.getElementsByClassName("about").item(0).style.display = "none";
+// }
+
+function showContainer() {
+    document.getElementsByClassName("container").item(0).style.display = "flex";
+    window.scrollTo({
+        top: 600,
+        behavior: "smooth",
+    });
 }
 
-function showAboutMe() {
-    document.getElementsByClassName("about").item(0).style.display = "inherit";
-}
-
-function hideAboutMe() {
-    document.getElementsByClassName("about").item(0).style.display = "none";
+function hideContainer() {
+    document.getElementsByClassName("container").item(0).style.display = "none";
 }
